52d0d6646a52d74b8aba87900cd088d2787bc239
Hepheir <hepheir@gmail.com>
2025-11-27 20:32:05 +0900

    feat: static 경로 추가
    Swagger UI, Admin 페이지 편의를 위하여 추가

--- app/urls.py
+++ app/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 import app.views
@@ -29,4 +31,7 @@ urlpatterns = [
     # Swagger Support
     path('swagger/', app.views.schema.with_ui('swagger')),
     path('swagger/(?P<format>\\.json|\\.yaml)', app.views.schema.without_ui()),
+
+    # Static Files Serving
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
