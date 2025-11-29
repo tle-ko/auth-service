86399eb403a2317e509bfc47079dd10e076ca9ac
Hepheir <hepheir@gmail.com>
2025-11-29 19:23:45 +0900

    feat(app.settings): `SECRET_KEY` 를 환경변수에서 가져오도록 변경

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ env.load(dotenv_path=BASE_DIR / '.env')
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^&757(fcj_5idk9lfj5%mc#5ozvfoigka&#z73lxc_(!4j_e$o'
+SECRET_KEY = env.get("SECRET_KEY", required=True)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
