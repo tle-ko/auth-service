e2b5004bccf97be0a04d9f8b4d26b76e826fb156
Hepheir <hepheir@gmail.com>
2025-11-29 11:23:41 +0900

    refactor(entrypoint): encapsulate `DEBUG` mode check in a separate function

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,15 @@
 set -e
 
 
+function is_debug_mode() {
+    # app/settings.py의 DEBUG 는 truthy falsy 값을 허용하므로,
+    # 해당 파싱 방식이 반영될 수 있도록 app/settings.py에서 직접 DEBUG 값을 관측.
+    echo 'from django.conf import settings; exit(0 if settings.DEBUG else 1);' \
+        | python manage.py shell --no-imports
+    return $?
+}
+
+
 function debug_init() {
     echo "[ENTRYPOINT] Running in DEBUG mode."
 
@@ -15,10 +24,9 @@ function debug_init() {
 function main() {
     echo "[ENTRYPOINT] $@"
 
-    # Check if DEBUG mode is enabled
-    echo 'from django.conf import settings; exit(0 if settings.DEBUG else 1);' \
-        | python manage.py shell --no-imports \
-        && debug_init
+    if is_debug_mode; then
+        debug_init
+    fi
 
     # Run server
     gunicorn app.wsgi:application "$@"
