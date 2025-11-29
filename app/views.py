211ee0148f6ea1863b0c1da6851e8f5981b897e6
Hepheir <hepheir@gmail.com>
2025-11-29 16:54:54 +0900

    fix(app.views): handle unexpected errors in `HealthCheckAPIView` instead of specific `DatabaseError`

--- app/views.py
+++ app/views.py
@@ -1,5 +1,4 @@
 from django.db import connections
-from django.db.utils import DatabaseError
 from django.db.utils import OperationalError
 from django.http import HttpRequest
 from django.http import HttpResponse
@@ -47,7 +46,7 @@ class HealthCheckAPIView(APIView):
                 pass
         except OperationalError:
             raise APIException("Database connection failed.")
-        except DatabaseError:
-            raise APIException("Database error occurred.")
+        except Exception:
+            raise APIException("Unexpected error during health check.")
 
         return HttpResponse(status=status.HTTP_200_OK)
