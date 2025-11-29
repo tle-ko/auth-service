d89f9d4042e8077694725c2ee0e968ba861f9056
Hepheir <hepheir@gmail.com>
2025-11-29 16:25:23 +0900

    refactor(entrypoint.sh): `main` 함수가 최상단으로 가도록 수정

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,15 @@
 set -e
 
 
-function is_debug_mode() {
-    # app/settings.py의 DEBUG 는 truthy falsy 값을 허용하므로,
-    # 해당 파싱 방식이 반영될 수 있도록 app/settings.py에서 직접 DEBUG 값을 관측.
-    echo 'from django.conf import settings; exit(0 if settings.DEBUG else 1);' \
-        | python manage.py shell --no-imports
+function main() {
+    echo "[ENTRYPOINT] Arguments: $@"
+
+    if is_debug_mode; then
+        debug_init
+    fi
+
+    # Run server
+    gunicorn app.wsgi:application "$@"
 }
 
 
@@ -22,15 +26,12 @@ function debug_init() {
 }
 
 
-function main() {
-    echo "[ENTRYPOINT] $@"
 
-    if is_debug_mode; then
-        debug_init
-    fi
-
-    # Run server
-    gunicorn app.wsgi:application "$@"
+function is_debug_mode() {
+    # app/settings.py의 DEBUG 는 truthy falsy 값을 허용하므로,
+    # 해당 파싱 방식이 반영될 수 있도록 app/settings.py에서 직접 DEBUG 값을 관측.
+    echo 'from django.conf import settings; exit(0 if settings.DEBUG else 1);' \
+        | python manage.py shell --no-imports
 }
 
 
