daa182d529efb3b7d49e7d1ae6b07ea629f78f13
Hepheir <hepheir@gmail.com>
2025-12-09 06:14:33 +0900

    feat(doc.views): implement OpenAPI schema views with Swagger and Redoc support

--- app/doc/views.py
+++ app/doc/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.conf import settings
+from drf_yasg.views import get_schema_view
 
-# Create your views here.
+
+SchemaView = get_schema_view(
+    info=settings.OPEN_API_INFO,
+    public=True,
+)
+
+
+redoc_view = SchemaView.with_ui('redoc')
+swagger_view = SchemaView.with_ui('swagger')
+schema_format_view = SchemaView.without_ui()
